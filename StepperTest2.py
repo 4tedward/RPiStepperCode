993eb675a73ebc70b25a65a418808299050a8413
4tedward <tedhermann@hotmail.com>
2018-08-11 09:21:04 -0400

    Update StepperTest2.py

--- StepperTest2.py
+++ StepperTest2.py
@@ -1,3 +1,5 @@
+# Operational Test for RPi SBC Stepper Hat 
+
 import PiMotor
 import time
 
@@ -5,7 +7,17 @@ m1 = PiMotor.Stepper("STEPPER1")
 
 # Rotate Stepper 1 Contiously in forward/backward direction
 while True:
-    m1.forward(0.1,10)  # Delay and rotations
+    m1.forward(0.1,20)  # Delay and rotations
     time.sleep(2)
-    m1.backward(0.1,10)
+    m1.backward(0.1,20)
+    time.sleep(2)
+    
+    m1.forward(0.1,100)  # Delay and rotations
+    time.sleep(2)
+    m1.backward(0.1,100)
+    time.sleep(2)
+    
+     m1.forward(0.01,600)  # Delay and rotations
+    time.sleep(2)
+    m1.backward(0.01,600)
     time.sleep(2)
